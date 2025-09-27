664e26e0956eaf75675d6cf5c7cf234a61cce154
Sam Wells <samwells95@gmail.com>
2025-09-26 18:01:41 -0700

    Add S3 config

---
+++ src/main/java/samwells/io/s3uploader/config/S3Config.java
@@ -0,0 +1,20 @@
+package samwells.io.s3uploader.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Bean
+    S3Client s3Client() {
+        return S3Client
+                .builder()
+                .region(Region.CA_CENTRAL_1)
+                 // Use AWS SSO CLI and custom profile - Sign in via CLI and the SDK handles the rest 😘
+                .credentialsProvider(ProfileCredentialsProvider.create("s3-uploader"))
+                .build();
+    }
+}
