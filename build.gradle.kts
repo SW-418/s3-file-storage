4a50a7389679eda3d73ac3397c804244bacd1747
Sam Wells <samwells95@gmail.com>
2025-09-26 18:01:32 -0700

    Add initial project

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "samwells.io"
+version = "0.0.1-SNAPSHOT"
+description = "Multipart uploading w/ S3"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    // API
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // S3
+    implementation("software.amazon.awssdk:s3:2.34.4")
+
+    // AWS SSO
+    implementation("software.amazon.awssdk:sso:2.34.4")
+    implementation("software.amazon.awssdk:ssooidc:2.34.4")
+
+    // Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+    // Testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/samwells/io/s3uploader/S3uploaderApplication.java
@@ -0,0 +1,11 @@
+package samwells.io.s3uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3uploaderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(S3uploaderApplication.class, args);
+	}
+}
